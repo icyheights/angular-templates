f622e4c045e7aaf586284c114bf1ffc5f4147978
icyheights <icyheights@users.noreply.github.com>
2015-10-06 13:39:13 +0300

    application test task

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,47 @@
-//Load gulp modules
+"use strict";
+
+//required node modules
 
 var gulp = require('gulp');
 var del = require('del');
 
-//Task configuration
+//Global configuration constants
+
+var TEMPLATES = 'src/templates/';
+var TARGET = 'target/';
+
+//Tasks
 
 var tasks = [
-	{name: 'directive', runner: buildDirective},
-	{name: 'service', runner: buildService},
-	{name: 'filter', runner: buildFilter},
+	{name: 'clean', deps: [], runner: clean},
+	{name: 'directive', deps: ['clean'], runner: directive},
+	{name: 'service', deps: ['clean'], runner: service},
+	{name: 'filter', deps: ['clean'], runner: filter},
 ];
-
-//Define base config
-
-var src = 'src';
-var target = 'target';
-
-//Define tasks
-
-gulp.task('clean', function() {
-	return del(target + '/**/*');
-});
 
 tasks.forEach(makeTask);
 
 gulp.task('default', function() {
-	process.stdout.write('\nAvailable gulps:\n');
+	process.stdout.write('\nAvailable tasks:\n');
 	tasks.forEach(printTask);
 	process.stdout.write('\n');
 });
 
-//Auxiliary functions
+//Task functions
 
 function makeTask(task) {
-	gulp.task(task.name, ['clean'], task.runner);
+	gulp.task(task.name, task.deps, task.runner);
 }
 
 function printTask(task) {
 	process.stdout.write('\t' + task.name + '\n');
 }
 
-function buildDirective() {
+function clean() {
+	return del(target + '**/*');
+}
+
+function directive() {
 	copyModule();
 	copyDirective();
 	copyController();
@@ -49,53 +50,55 @@ function buildDirective() {
 	copyTest();
 }
 
-function buildService() {
+function service() {
 	copyModule();
 	copyService();
 }
 
-function buildFilter() {
+function filter() {
 	copyModule();
 	copyFilter();
 }
 
+//Auxiliary functions
+
 function copyDirective() {
-	gulp.src(src + '/directive.js').
-		pipe(gulp.dest(target));
+	gulp.src(TEMPLATES + 'directive.js')
+		.pipe(gulp.dest(TARGET));
 }
 
 function copyCSS() {
-	gulp.src(src + '/style.css').
-		pipe(gulp.dest(target));
+	gulp.src(TEMPLATES + 'style.css')
+		.pipe(gulp.dest(TARGET));
 }
 
 function copyController() {
-	gulp.src(src + '/controller.js').
-		pipe(gulp.dest(target));
+	gulp.src(TEMPLATES + 'controller.js')
+		.pipe(gulp.dest(TARGET));
 }
 
 function copyModule() {
-	gulp.src(src + '/module.js').
-		pipe(gulp.dest(target));
+	gulp.src(TEMPLATES + 'module.js')
+		.pipe(gulp.dest(TARGET));
 }
 
 function copyFilter() {
-	gulp.src(src + '/filter.js').
-		pipe(gulp.dest(target));
+	gulp.src(TEMPLATES + 'filter.js')
+		.pipe(gulp.dest(TARGET));
 }
 
 function copyService() {
-	gulp.src(src + '/service.js').
-		pipe(gulp.dest(target));
+	gulp.src(TEMPLATES + 'service.js')
+		.pipe(gulp.dest(TARGET));
 }
 
 function copyTemplate() {
-	gulp.src(src + '/template.html').
-		pipe(gulp.dest(target));
+	gulp.src(TEMPLATES + 'template.html')
+		.pipe(gulp.dest(TARGET));
 }
 
 function copyTest() {
-	gulp.src(src + '/test*').
-		pipe(gulp.dest(target));
+	gulp.src(TEMPLATES + 'test*')
+		.pipe(gulp.dest(TARGET + '_test'));
 }
 
--- src/application-structure/gulpfile.js
+++ src/application-structure/gulpfile.js
@@ -1,59 +1,159 @@
-//Copy to your project's gulpfile.js
+"use strict";
 
-//Prerequisites
+//Required node modules
+
 var gulp = require('gulp');
-var gulp-rename = require('gulp-rename');
+var del = require('del');
+var argv = require('yargs').argv;
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify'); 
+var angularFilesort = require('gulp-angular-filesort');
+var concat = require('gulp-concat');
+var minifyCSS = require('gulp-minify-css');
+var rename = require('gulp-rename');
 
-// Directive test task
-(function() {
-	var taskSettings = { 
-		//change according to the project, if necessary
-		taskName: 'test-directive',
-		srcRoot: 'src',
-		targetRoot: 'target',
-		bowerComponents: 'bower_components',
-		cleanTargetTaskName: 'clean'
-	};
+//Global configuration constants
 
-	gulp.task(taskSettings.taskName, [taskSettings.cleanTargetTaskName], buildTest);
+var TARGET = 'target/';
 
-	function buildTest() {
-		if (process.argv[3]) {
-			var directiveName = process.argv[3].substring(2);
-			var directivePath = taskSettings.srcRoot + '/' + directiveName.replace(/-/g, '/');
+//Tasks
 
-			gulp.src([
-					directivePath + '/*.js',
-					directivePath + '/*.css'
-				]).
-				pipe(gulp.dest(taskSettings.targetRoot));
+var tasks = [
+	{name: 'clean', deps: [], runner: clean},
+	{name: 'test', deps: ['clean'], runner: test},
+	{name: 'example', deps: ['clean'], runner: test},
+];
 
-			gulp.src(taskSettings.bowerComponents + '/**/*').
-				pipe(gulp.dest(taskSettings.targetRoot));
+tasks.forEach(function(task) {
+	gulp.task(task.name, task.deps, task.runner);
+});
 
-			gulp.src(directivePath + '/template.html').
-				pipe(rename({
-					dirname: '',
-					basename: directiveName
-				})).
-				pipe(gulp.dest(taskSettings.targetRoot + '/templates'));
+gulp.task('default', function() {
+	process.stdout.write('\nAvailable tasks:\n');
+	tasks.forEach(function(task) {
+		process.stdout.write('\t' + task.name + '\n');
+	});
+	process.stdout.write('\n');
+});
 
-			gulp.src(directivePath + '/test-template.html').
-				pipe(rename({
-					dirname: '',
-					basename: 'test'
-				})).
-				pipe(gulp.dest(taskSettings.targetRoot + '/templates'));
+//Task functions
 
-			gulp.src(directivePath + '/test-index.html').
-				pipe(rename({
-					dirname: '',
-					basename: 'index'
-				})).
-				pipe(gulp.dest(taskSettings.targetRoot));
-		} else {
-			process.stdout.write('\nError: directive name unspecified\n');
-			process.stdout.write('Usage: gulp ' + taskSettings.taskName + ' --{directive-name}\n');
+function clean() {
+	return del(config.target.base + '**/*');
+}
+
+function test() {
+	var moduleName = config.argv.n;
+	if (!moduleName) {
+		process.stdout.write('Incorrect invocation.\n');
+		process.stdout.write('Usage: gulp test -n module.name\n');
+	}
+
+	buildDirective(moduleName);
+	copyResources();
+	copyLibs(require('src/test/lib.json'));
+	copyTestDirective(moduleName);
+	copyTestIndex();
+}
+
+//Auxiliary functions
+
+function buildDirective(moduleName) {
+	var moduleNames = getAllModuleNames(moduleName);
+	
+	buildJS(moduleNames);
+	buildCSS(moduleNames);
+	buildTemplates(moduleNames);
+}
+
+function getModulePath(moduleName) {
+	return 'src/components/' + moduleName.replace(/\./g, '/') + '/';
+}
+
+function getAllModuleNames(moduleName) {
+	var directDependencies = require(getModulePath(moduleName) + 'dependencies.json');
+	return Array.prototype.concat.apply([moduleName], directDependencies.map(getAllPaths));
+}
+
+function glob(moduleName, pattern) {
+	return getModulePath(moduleName) + pattern;
+}
+
+function buildJS(moduleNames) {
+	var stream = gulp.src(moduleNames.map(jsGlob))
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'))
+		.pipe(angularFilesort())
+		.pipe(concat('script.js'));
+	
+	if (!args.dev) {
+		stream = stream.pipe(uglify());
+	}
+
+	stream.pipe(gulp.dest(TARGET));
+
+	function jsGlob(moduleName) {
+		return glob(moduleName, '*.js');
+	}
+}
+
+function buildCSS(moduleNames) {
+	gulp.src(moduleNames.map(cssGlob))
+		.pipe(concat('style.css'))
+		.pipe(minifyCSS())
+		.pipe(gulp.dest(TARGET));
+
+	function cssGlob(moduleName) {
+		return glob(moduleName, 'style.css');
+	}
+}
+
+function buildTemplates(moduleNames) {
+	moduleNames.forEach(buildTemplate);
+
+	function buildTemplate(moduleName) {
+		gulp.src(glob(moduleName, 'template.html'))
+			.pipe(rename({
+				dirname: 'templates',
+				basename: templateName(moduleName) 
+			}))
+			.pipe(gulp.dest(TARGET));
+
+		function templateName(moduleName) {
+			return moduleName.replace(/\./g, '-');
 		}
 	}
-})();
+}
+
+function copyResources() {
+	gulp.src('resources/**/*')
+		.pipe(gulp.dest(TARGET));
+}
+
+function copyLibs(libConfig) {
+	libConfig.map(copyLib);
+
+	function copyLib(libConfigEntry) {
+		var source = argv.dev ? libConfigEntry.source.development : libConfigEntry.source.production;
+		gulp.src(source)
+			.pipe(gulp.rename({
+				dirname: 'lib',
+				basename: libConfigEntry.targetName
+			}))
+			.pipe(gulp.dest(TARGET));
+	}
+}
+
+function getBuildVersion() {
+	return argv.dev ? 'Development' : 'Production';
+}
+
+function copyTestDirective(moduleName) {
+	gulp.src(getModulePath(moduleName) + '_test/*')
+		.pipe(gulp.dest(TARGET));
+}
+
+function copyTestIndex() {
+	gulp.src('src/test/index.html')
+		.pipe(gulp.dest(TARGET));
+}
