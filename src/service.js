30c5a2f93475b49858827418ecae2ab74b8c911a
Roman.Utochka <Roman.Utochka@ocrv.ru>
2015-11-04 18:51:55 +0300

    fix service module defintion

--- src/service.js
+++ src/service.js
@@ -6,8 +6,8 @@
 (function() {
 	'use strict';
 
-	angular.module('__name-pattern__')
-		.factory('__namePattern__', __namePattern__);
+	angular.module('__name-pattern__', [
+	]).factory('__namePattern__', __namePattern__);
 	
 	__namePattern__.$inject = [];
 
